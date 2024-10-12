5b5f0d6e2416652f65666e80c85cd37cfded4977
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2024-10-12 19:15:07 +0530

    routing issue solved

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ const Intro = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('Login');
+      router.replace('/screens/Login');
     }, 2000);
 
     return () => clearTimeout(timer);
@@ -58,3 +58,20 @@ const styles = StyleSheet.create({
 });
 
 export default Intro;
+
+
+// import { Stack } from "expo-router";
+
+// const RootLayout = () => {
+//   return (
+//     <Stack >
+//       <Stack.Screen name="screens/Intro" options={{headerShown: false}} />
+//       <Stack.Screen name="screens/Login" options={{headerShown: false}} />
+//       <Stack.Screen name="screens/Signup" options={{headerShown: false}} />
+//       <Stack.Screen name="screens/DescribeRole" options={{headerShown: false}} />
+//       <Stack.Screen name="screens/Home" options={{headerShown: false}} />
+//     </Stack>
+//   );
+// };
+
+// export default RootLayout;
