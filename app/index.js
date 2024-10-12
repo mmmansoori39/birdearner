837cdba53e6a05e82585fdaf6249853d12afedfa
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-10-12 19:56:29 +0530

    removed comments

--- app/index.js
+++ app/index.js
@@ -58,20 +58,3 @@ const styles = StyleSheet.create({
 });
 
 export default Intro;
-
-
-// import { Stack } from "expo-router";
-
-// const RootLayout = () => {
-//   return (
-//     <Stack >
-//       <Stack.Screen name="screens/Intro" options={{headerShown: false}} />
-//       <Stack.Screen name="screens/Login" options={{headerShown: false}} />
-//       <Stack.Screen name="screens/Signup" options={{headerShown: false}} />
-//       <Stack.Screen name="screens/DescribeRole" options={{headerShown: false}} />
-//       <Stack.Screen name="screens/Home" options={{headerShown: false}} />
-//     </Stack>
-//   );
-// };
-
-// export default RootLayout;
