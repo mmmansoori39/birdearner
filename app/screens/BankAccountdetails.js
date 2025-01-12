e5f248b7ae35d80c8833ca1e0f70a2da4d2d4888
mmmansoori39 <mdmoinuddinmansoori@gmail.com>
2025-01-12 18:46:04 +0530

    secured account number details

--- app/screens/BankAccountdetails.js
+++ app/screens/BankAccountdetails.js
@@ -10,15 +10,17 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { appwriteConfig, databases } from "../lib/appwrite";
 import { useAuth } from "../context/AuthContext";
-import Toast from 'react-native-toast-message';
+import Toast from "react-native-toast-message";
 
-const BankAccountdetailsScreen = ({ navigation }) => {
+const BankAccountDetailsScreen = ({ navigation }) => {
   const [bankName, setBankName] = useState("");
   const [accountHolderName, setAccountHolderName] = useState("");
   const [accountNumber, setAccountNumber] = useState("");
+  const [isEditingAccountNumber, setIsEditingAccountNumber] = useState(false);
   const [confirmAccountNumber, setConfirmAccountNumber] = useState("");
   const [ifscCode, setIfscCode] = useState("");
-  const { userData } = useAuth()
+  const [isEditingIfscCode, setIsEditingIfscCode] = useState(false);
+  const { userData } = useAuth();
 
   useEffect(() => {
     fetchBankDetails();
@@ -26,40 +28,47 @@ const BankAccountdetailsScreen = ({ navigation }) => {
 
   const handleError = (message) => {
     Toast.show({
-      type: 'error',
-      text1: 'Error',
+      type: "error",
+      text1: "Error",
       text2: message,
     });
   };
 
   const handleSuccess = (message) => {
     Toast.show({
-      type: 'success',
-      text1: 'Success',
+      type: "success",
+      text1: "Success",
       text2: message,
     });
   };
 
+  const maskValue = (value) => {
+    if (!value || value.length <= 4) return value;
+    return "x".repeat(value.length - 4) + value.slice(-4);
+  };
+
   const fetchBankDetails = async () => {
     try {
-
       if (userData) {
         setBankName(userData?.bankName || "");
         setAccountHolderName(userData?.accountHolderName || "");
+
         const accountNumber = userData?.accountNumber;
         setAccountNumber(accountNumber ? accountNumber.toString() : "");
-        setConfirmAccountNumber(accountNumber ? accountNumber.toString() : "");
-        setIfscCode(userData?.ifscCode || "");
+        const ifsc = userData?.ifscCode;
+        setIfscCode(ifsc || "");
       }
     } catch (error) {
-      handleError("Error fetching bank details")
+      handleError("Error fetching bank details");
     }
   };
 
   const handleSave = async () => {
     if (accountNumber === confirmAccountNumber) {
-
-      const collectionId = userData?.role === "client" ? appwriteConfig.clientCollectionId : appwriteConfig.freelancerCollectionId
+      const collectionId =
+        userData?.role === "client"
+          ? appwriteConfig.clientCollectionId
+          : appwriteConfig.freelancerCollectionId;
 
       try {
         const document = {
@@ -75,19 +84,19 @@ const BankAccountdetailsScreen = ({ navigation }) => {
           document
         );
 
-        handleSuccess("Bank Details Saved successfully!")
-        // navigation.goBack();
+        handleSuccess("Bank Details Saved successfully!");
+        navigation.goBack();
       } catch (error) {
-        handleError("Error saving bank details")
+        handleError("Error saving bank details");
       }
     } else {
-      handleError("Account numbers do not match!")
+      handleError("Account numbers do not match!");
     }
   };
 
   return (
     <ScrollView style={styles.container}>
-      <View >
+      <View>
         <View style={styles.main}>
           <TouchableOpacity
             onPress={() => navigation.goBack()}
@@ -95,9 +104,7 @@ const BankAccountdetailsScreen = ({ navigation }) => {
           >
             <Ionicons name="arrow-back" size={24} color="black" />
           </TouchableOpacity>
-          <Text style={styles.header}>
-            Bank Account details
-          </Text>
+          <Text style={styles.header}>Bank Account details</Text>
         </View>
 
         <Text style={styles.label}>Select your bank</Text>
@@ -120,9 +127,16 @@ const BankAccountdetailsScreen = ({ navigation }) => {
         <TextInput
           style={styles.input}
           placeholder="Enter account number"
-          value={accountNumber}
-          onChangeText={setAccountNumber}
+          value={
+            isEditingAccountNumber ? accountNumber : maskValue(accountNumber)
+          }
+          onFocus={() => {
+            setIsEditingAccountNumber(true);
+            setAccountNumber("");
+          }}
+          onChangeText={(text) => setAccountNumber(text)}
           keyboardType="numeric"
+          secureTextEntry={isEditingAccountNumber}
         />
 
         <Text style={styles.label}>Confirm your account number</Text>
@@ -132,14 +146,20 @@ const BankAccountdetailsScreen = ({ navigation }) => {
           value={confirmAccountNumber}
           onChangeText={setConfirmAccountNumber}
           keyboardType="numeric"
+          secureTextEntry
         />
 
         <Text style={styles.label}>Enter your bank IFSC code</Text>
         <TextInput
           style={styles.input}
           placeholder="Enter IFSC code"
-          value={ifscCode}
-          onChangeText={setIfscCode}
+          value={isEditingIfscCode ? ifscCode : maskValue(ifscCode)}
+          onFocus={() => {
+            setIsEditingIfscCode(true);
+            setIfscCode("");
+          }}
+          onChangeText={(text) => setIfscCode(text)}
+          secureTextEntry={isEditingIfscCode}
         />
 
         <TouchableOpacity style={styles.signupButton} onPress={handleSave}>
@@ -162,9 +182,7 @@ const styles = StyleSheet.create({
   main: {
     marginTop: 45,
     marginBottom: 50,
-    display: "flex",
     flexDirection: "row",
-    gap: 40,
     alignItems: "center",
   },
   header: {
@@ -187,7 +205,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginBottom: 20,
     fontSize: 14,
-    color: "#898686"
+    color: "#898686",
   },
   signupButton: {
     width: "40%",
@@ -197,7 +215,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 20,
-    margin: "auto",
   },
   signupButtonText: {
     color: "white",
@@ -206,4 +223,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default BankAccountdetailsScreen;
+export default BankAccountDetailsScreen;
