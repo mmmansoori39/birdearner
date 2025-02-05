002e1b1a82776a728e7f6f1870e3c960bfd5aed8
mmmansoori39 <mdmoinuddinmansoori@gmail.com>
2025-02-05 12:16:58 +0530

    navigator code fixed

--- app/_layout.js
+++ app/_layout.js
@@ -3,17 +3,15 @@ import { AuthProvider } from './context/AuthContext';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from './context/ThemeContext';
-import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <ThemeProvider>
+    <ThemeProvider>
         <AuthProvider>
           <Slot />
           <StatusBar style="auto" />
         </AuthProvider>
       </ThemeProvider>
-    </NavigationContainer>
   );
 }
